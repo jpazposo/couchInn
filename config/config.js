24926d21d6c42a714b1e88a2f887bf6491c0d449
Luciano Perez Cerra <luchopcerra@gmail.com>
2016-03-18 20:46:59 -0300

    Primer commit: scaffold de una aplicacion nodeJS con express  https://github.com/petecoop/generator-express

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'couchinn'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/couchinn-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'couchinn'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/couchinn-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'couchinn'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/couchinn-production'
+  }
+};
+
+module.exports = config[env];
