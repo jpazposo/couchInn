c16f5916d8c7d3d4f5a68c7f9ea8a66f2de040c1
Luciano Perez Cerra <luchopcerra@gmail.com>
2016-06-03 18:12:06 -0300

    agregué validacion de usuario ya registrado

--- public/app/register/src/register-controller.js
+++ public/app/register/src/register-controller.js
@@ -4,7 +4,8 @@ angular.module('register').controller(
     [
         '$scope',
         'couchinnService',
-        function ($scope, couchinnService) {
+        '$location',
+        function ($scope, couchinnService, $location) {
           console.log('se cargó el controller registerController');
 
           $scope.user = {}; // modelo a completarse con el formulario.
@@ -16,7 +17,7 @@ angular.module('register').controller(
               name: 'Iniciar Sessión'
             }
           ];
-          
+
           $scope.register = function () {
             console.log('se va a registrar a este usuario:-----------');
             console.log(JSON.stringify($scope.user));
@@ -25,10 +26,19 @@ angular.module('register').controller(
               .then(function (user) {
                 console.log('successfull registration of : ----------------');
                 console.log(JSON.stringify(user));
+                $location.url('/login');
               })
               .catch(function (error) {
 
                 // code 11000 means user already exist
+                alert("hubo un error registrando al usuario: " +
+                  JSON.stringify($scope.user.nombre)
+                );
+                if (error.data.code == 11000) {
+                  alert(
+                    "Correo Electrónico ya registrado"
+                  );
+                }
                 console.log(error);
               });
           }
